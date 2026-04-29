b1052a9a9275954ba2cd36c28706eeda91c2fe1b
ZeroTwo020202020202 <pokemon1234mm1@gmail.com>
2026-04-29 23:15:18 +0530

    Change message to celebrate Anusha's birthday
    Updated the message displayed in the message box to a more positive sentiment.

--- file/functions.js
+++ file/functions.js
@@ -1,5 +1,3 @@
-
-
 // variables
 var $win = $(window);
 var clientWidth = $win.width();
@@ -40,23 +38,27 @@ function timeElapse(date){
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
+
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
+
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
+
 	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
+
 	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
 	$("#clock").html(result);
 
-	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
+	// ❤️ Updated message (Anusha added, unwanted line removed)
+	var text = "The world got more beautiful the day Anusha was born 💖✨";
 	$("#message-box").html(text);
-
 }
